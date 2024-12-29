862cc7251c3fa502de23939b8a540273acfda67c
Vishvak Gurram <gurram.vishvak@gmail.com>
2024-12-28 21:05:26 -0800

    Adding modified app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask,redirect,url_for,render_template
 app=Flask(__name__)
-@app.route("/home")
+@app.route("/")
 def home():
     return render_template('index.html')
 @app.route("/robot1")
